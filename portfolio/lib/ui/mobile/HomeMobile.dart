76ad809fc6222260af227dfe38a0727f91a77245
kafri8889 <kafri8889@gmail.com>
2023-08-21 15:26:39 +0700

    Mobile home ui update

--- portfolio/lib/ui/mobile/HomeMobile.dart
+++ portfolio/lib/ui/mobile/HomeMobile.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../controller/HomeController.dart';
@@ -99,47 +100,76 @@ class HomeMobile extends StatelessWidget {
                       "svg/android.svg"
                   )
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 72, right: 72),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                        "Hello! I am",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 48,
-                          fontWeight: FontWeight.w700,
-                          height: 1.08,
-                        )
-                    ),
-                    AnimatedTextKit(
-                        repeatForever: true,
-                        animatedTexts: [
-                          TypewriterAnimatedText(
-                              "Anaf Naufalian",
-                              speed: const Duration(milliseconds: 128),
-                              textStyle: TextStyle(
-                                color: Theme.of(context).primaryColor,
+              FittedBox(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 72, right: 72),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(width: 0.0, height: 16.0),
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 48,
+                            child: DefaultTextStyle(
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 48,
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.08,
+                                ),
+                                child: AnimatedTextKit(
+                                    repeatForever: true,
+                                    animatedTexts: [
+                                      RotateAnimatedText("Hello", duration: const Duration(seconds: 2), rotateOut: false),
+                                      RotateAnimatedText("안녕하세요", duration: const Duration(seconds: 2), rotateOut: false),
+                                      RotateAnimatedText("こんにちは", duration: const Duration(seconds: 2), rotateOut: false),
+                                      RotateAnimatedText("你好", duration: const Duration(seconds: 2), rotateOut: false),
+                                    ]
+                                )
+                            ),
+                          ),
+                          const Text(
+                              "! I am",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
                                 fontSize: 48,
-                                fontFamily: 'Inter',
                                 fontWeight: FontWeight.w700,
                                 height: 1.08,
                               )
+                          ),
+                        ],
+                      ),
+                      AnimatedTextKit(
+                          repeatForever: true,
+                          animatedTexts: [
+                            TypewriterAnimatedText(
+                                "Anaf Naufalian",
+                                speed: const Duration(milliseconds: 128),
+                                textStyle: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 48,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.08,
+                                )
+                            )
+                          ]
+                      ),
+                      const Padding(padding: EdgeInsets.all(16)),
+                      const Text(
+                          "i'm an Android Developer 📱 based in Indonesia",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400
                           )
-                        ]
-                    ),
-                    const Padding(padding: EdgeInsets.all(16)),
-                    const Text(
-                        "i'm an Android Developer 📱 based in Indonesia",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w400
-                        )
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
               const Padding(padding: EdgeInsets.all(16)),
@@ -150,7 +180,7 @@ class HomeMobile extends StatelessWidget {
                 color: Theme.of(context).colorScheme.surfaceVariant,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     const Padding(padding: EdgeInsets.all(16)),
                     Text(
@@ -163,6 +193,86 @@ class HomeMobile extends StatelessWidget {
                         fontWeight: FontWeight.w700,
                         height: 1.08,
                       ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(24)),
+                    Obx(() =>
+                        AnimatedOpacity(
+                          duration: const Duration(seconds: 1),
+                          opacity: controller.textOpacity.value,
+                          child: Text(
+                            controller.currentProject.value.name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                              height: 1.22,
+                            ),
+                          ),
+                        )
+                    ),
+                    const Padding(padding: EdgeInsets.all(16)),
+                    FittedBox(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            borderRadius: const BorderRadius.all(Radius.circular(100)),
+                            onTap: () {
+                              controller.previous();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: SvgPicture.asset(
+                                "svg/arrow_left_new.svg",
+                                width: 32,
+                                height: 32,
+                              ),
+                            ),
+                          ),
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(48),
+                              child: Obx(() =>
+                                  Image.asset(
+                                      controller.currentProject.value.imgPath,
+                                      scale: 3,
+                                      filterQuality: FilterQuality.low,
+                                      isAntiAlias: true
+                                  )
+                              )
+                          ),
+                          InkWell(
+                            borderRadius: const BorderRadius.all(Radius.circular(100)),
+                            onTap: () {
+                              controller.next();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: SvgPicture.asset(
+                                "svg/arrow_right_new.svg",
+                                width: 32,
+                                height: 32,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(16)),
+                    Wrap(
+                      children: [
+                        Obx(() =>
+                            FilledButton(
+                                onPressed: () async {
+                                  if (!await launchUrl(Uri.parse(controller.currentProject.value.url))) {
+                                    throw Exception('Could not launch url');
+                                  }
+                                },
+                                child: Text(controller.currentProject.value.type.name)
+                            )
+                        )
+                      ],
                     ),
                     const Padding(padding: EdgeInsets.all(16)),
                   ],
